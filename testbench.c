4305abaf8627d0b74a38f7b442615832ff428680
Arghya10 <60973281+Arghya10@users.noreply.github.com>
2020-10-22 10:49:12 +0530

    Create testbench.c

---
+++ testbench.c
@@ -0,0 +1,15 @@
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthreadUtils.h>
+#include <Pipes.h>
+#include <pipeHandler.h>
+
+
+
+int main()
+{
+        return 0;
+}
