1c8be1208145eaaaa6a73c95823a7c48d8696496
Priyankar Sarkar <priyansrk@gmail.com>
2020-10-23 12:43:41 +0530

    Update testbench.c

--- SD/testbench.c
+++ SD/testbench.c
@@ -19,10 +19,27 @@ typedef bool uint1_t;
 #define resp0 0x10
 #define resp2 0x14
 #define resp4 0x18
-#define resp6 0x1e
+#define resp6 0x1c
 #define bufferdata 0x20
 #define present 0x24
+#define hostcontrol 0x28
+#define powercontrol 0x29
+#define blockgapcontrol 0x2A
+#define wakeupcontrol 0x2B
+#define clockcontrol 0x2C
+#define timeoutcontrol 0x2E
+#define swreset 0x2F
+#define nintrstatus 0x30
+#define eintrstatus 0x32
+#define nintrstatusen 0x34
+#define eintrstatusen 0x36
+#define nintrsignalen 0x38
+#define eintrsignalen 0x3A
+#define autocmderror 0x3C
+#define hostcontrol2 0x3E
 #define capa 0x40
+#define maxIcap 0x48
+#define HCversion 0xFE
 
 
 #define SD_Base 0x00//SD base address
