8f17d8ab890592442ec6276a23e7fd0d6fdfa571
Arghya10 <arghyakamaldey10@gmail.com>
2021-01-19 20:50:29 +0530

    New testbench

---
+++ SD/testbench/testbench.c
@@ -0,0 +1,25 @@
+//main ()
+//{
+//
+//      
+//      initialization(0)//for normal initialization
+//      blockwrite()
+//      blockread()
+//
+//      swreset()
+//      initialization(1)//for UHS initialization
+//      blockwrite()
+//      blockread()
+//
+//      }
+//
+//      readwriteSdhcRegisters()
+//      SendCMD()
+//      SendACMD()
+//      checkInterrupt()
+//      interrupt_check() xxxx
+//      clear_interrupt xxxxx
+//      casefunc() xxxxx
+//      checkDATline()
+//
+
